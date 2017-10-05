1bef22c3b4a2daf631c37abf4267d3137d7a73b4
olson <darcy.olson@ties.k12.mn.us>
2017-10-05 12:40:09 -0500

    this isn’t working

--- xvfb-run.sh
+++ xvfb-run.sh
@@ -20,12 +20,10 @@ STARTWAIT=3
 SCREEN_WIDTH=${SCREEN_WIDTH:-'1280'}
 SCREEN_HEIGHT=${SCREEN_HEIGHT:-'800'}
 XVFBARGS="-screen 0, ${SCREEN_WIDTH}x${SCREEN_HEIGHT}x24"
-
-
 LISTENTCP="-nolisten tcp"
 XAUTHPROTO=.
 
-#echo "Using screen size ${SCREEN_WIDTH}x${SCREEN_HEIGHT}"
+echo "Using screen size ${SCREEN_WIDTH}x${SCREEN_HEIGHT}"
 # Query the terminal to establish a default number of columns to use for
 # displaying messages to the user.  This is used only as a fallback in the event
 # the COLUMNS variable is not set.  ($COLUMNS can react to SIGWINCH while the
